83c8c7449d4d8d5728d319bee6a9edb89dd78f2b
Akshay <akshay.law35@gmail.com>
2023-02-21 06:25:39 +0000

    project

---
+++ OfficeEmp/OfficeEmp/Data/OfficeEmpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OfficeEMP.Models;
+
+namespace OfficeEmp.Data
+{
+    public class OfficeEmpContext : DbContext
+    {
+        public OfficeEmpContext (DbContextOptions<OfficeEmpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<OfficeEMP.Models.Employee> Employee { get; set; } = default!;
+    }
+}
---
+++ OfficeEmp/OfficeEmp/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OfficeEMP.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime JoinDate { get; set; }
+    public string? Department { get; set; }
+    public decimal Salary { get; set; }
+}
